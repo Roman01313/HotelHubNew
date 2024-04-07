2407be168b0b712628ad91d53d6eca6379de5042
Roman <129669519+Roman01313@users.noreply.github.com>
2024-04-07 16:49:55 +0300

    js comment + search system

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
-let container = document.querySelector(".container")let search = document.querySelector(".search")
+let container = document.querySelector(".container")
+let search = document.querySelector(".search")
 
 class Hotel{
     constructor(pict1, name, rait, desc, price ){
@@ -14,13 +15,14 @@ class Hotel{
         <img class="Img" src=${this.pict1}>
         <div class="info">
             <p>${this.desc}</p>
-            <p>${this.rait}</p>            <p>${this.price}$/week</p>
+            <p>${this.rait}</p>            
+            <p>${this.price}$/week</p>
         </div>  
     </div> -->`
     }
 }
 
-
+//Создание внутренностей карточек Отеля через Js 
 let hemer = new Hotel ("src/IMG_8244.JPG", "Hemer","9/10", "Hemer - the best hotel in Canada", 900)
 let tyfoul = new Hotel("src/IMG_8247.JPG", "Tyfoul", "5/10", "Tyfoul - not bad place to rest your time , rooms are so comfortable", 650)
 let gromur = new Hotel("src/IMG_8254.JPG", "Gromur", "10/10", "Gromur - is very comfortable place in New York, this hotel is very exited for mane people", 850)
@@ -29,6 +31,14 @@ let hotel_dict = [hemer, tyfoul, gromur, boston]
 
 
 for (let i = 0; i < hotel_dict.length; i += 1){
-    container.innerHTML += hotem_dict[i].toHtml()
+    container.innerHTML += hotem_dict[i].toHtml()//Добавление карточек отелей в контейнер на экране
 }
 
+function search_func(){
+    for(let i = 0; i <= hotel_dict; i += 1){
+        if(search.value == hotel_dict[i].name){
+            console.log('found it')//Ну вообщем дальше в массиве нужно поменять оюбекты местами,удалить их с экрана в контейнере и после перерисовать их на жкране в новом порядке
+            //hotel_dict[0] == search.value
+        }
+    }
+}
