49bd16863728b23f7a46bc60ebaf859c41baef56
Roman <129669519+Roman01313@users.noreply.github.com>
2024-04-07 16:11:38 +0300

    js 1/4

--- script2.js
+++ script2.js
@@ -1,1 +1,27 @@
-document.querySelector("button").innerHTML = "ooo"
+let container = document.querySelector(".container")
+
+class Hotel{
+    constructor(pict1, name, rait, desc, price ){
+        this.pict1 = pict1 
+        this.name = name
+        this.rait = rait
+        this.desc = desc
+        this.price = price
+    }
+    toHtml(){
+        return `<!-- <div class="hotel_card">
+        <h1>Hymer</h1>
+        <img class="Img" src=${this.pict1}>
+        <div class="info">
+            <p>${this.desc}</p>
+            <p>${this.rait}</p>
+            <p>${this.price}</p>
+        </div>  
+    </div> -->`
+    }
+}
+
+let hemer_price = 900
+let Hemer = new Hotel ("src/IMG_8244.JPG", "Hemer","9/10", "Hemer - the best hotel in Canada",`${hemer_price}$/week`)
+
+container.innerHTML += Hemer.toHtml()
